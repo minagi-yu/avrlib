d2b984cd8f8b12804ab0bc4661e76c879875f789
Minagi Yu <39975793+minagi-yu@users.noreply.github.com>
2019-12-08 19:03:52 +0900

    送信関数だいたい完成

--- twi/i2c.c
+++ twi/i2c.c
@@ -1,7 +1,7 @@
 #include "i2c.h"
 
 #define I2C_CLK_PER 3333333
-#define I2C_FSCL    I2C_SM
+#define I2C_FSCL I2C_SM
 
 void i2c_init(int khz)
 {
@@ -10,30 +10,56 @@ void i2c_init(int khz)
     TWI0.MCTRLA = (1 << TWI_ENABLE_bm);
 }
 
+static void i2c_wait()
+{
+    while (!(TWI0.MSTATUS & TWI_RIF_bm) && !(TWI0.MSTATUS & TWI_WIF_bm))
+        ;
+    TWI0.MSTATUS |= (TWI_RIF_bm | TWI_WIF_bm);
+}
+
+static void i2c_reset()
+{
+    TWI0.MCTRLB |= TWI_FLUSH_bm;
+    TWI0.MSTATUS |= TWI_BUSSTATE_IDLE_gc;
+}
+
 int i2c_send(uint8_t addr, void *data, size_t len)
 {
-    TWI0.MCTRLB  |= TWI_FLUSH_bm;
-	TWI0.MSTATUS |= TWI_BUSSTATE_IDLE_gc;
-	TWI0.MSTATUS |= (TWI_RIF_bm | TWI_WIF_bm);
+    uint8_t *dp = data;
 
-// M2
-    TWI0.MADDR = addr << 1;
+    if (len == 0)
+        return 1;
 
-    while (!(TWI0.MSTATUS & TWI_WIF_bm));
+    i2c_reset();
+    TWI0.MSTATUS |= (TWI_RIF_bm | TWI_WIF_bm);
 
-    if (TWI0.MSTATUS & TWI_ARBLOST_bm) {
-        TWI0.MCTRLB |= TWI_FLUSH_bm;
-        TWI0.MSTATUS |= TWI_BUSSTATE_IDLE_gc;
-        return 1; //fail
-        // M1
-    }
+    do {
+        TWI0.MADDR = addr << 1;
 
-    if (TWI0.MSTATUS & TWI_RXACK_bm) { //NACK
-        TWI0.MCTRLB |= TWI_MCMD_STOP_gc;
-        // goto M2
-    }
+        i2c_wait();
 
-    // receive ACK
-    
+        if (TWI0.MSTATUS & TWI_ARBLOST_bm) {
+            i2c_reset();
+            return 1;
+        }
+    } while (TWI0.MSTATUS & TWI_RXACK_bm); // NACK: rewrite address
 
+    // ACK: write data
+    do {
+        TWI0.MDATA = *dp++;
+
+        i2c_wait();
+
+        if (TWI0.MSTATUS & TWI_ARBLOST_bm) {
+            i2c_reset();
+            return 1;
+        }
+
+        if (TWI0.MSTATUS & TWI_RXACK_bm)
+            break;
+    } while (--len);
+
+    TWI0.MCTRLB |= TWI_MCMD_STOP_gc;
+
+    return 0;
 }
