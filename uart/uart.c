a58767faafe5b1cff378fd65a78a64b19802878f
Minagi Yu <39975793+minagi-yu@users.noreply.github.com>
2019-12-11 22:22:30 +0900

    だいたい完成

--- uart/uart.c
+++ uart/uart.c
@@ -7,14 +7,14 @@
 #define UART_BAUD 9600
 
 #define UART_TX_PORT PORTA
-#define UART_TX_PIN PIN6_bp
+#define UART_TX_PIN 6
 #define UART_RX_PORT PORTA
-#define UART_RX_PIN PIN7_bp
+#define UART_RX_PIN 7
 
 static volatile struct uart_buffer {
-    uint16_t ridx;
-    uint16_t widx;
-    uint16_t cnt;
+    uint8_t ridx;
+    uint8_t widx;
+    uint8_t cnt;
     uint8_t buff[UART_BUFFER_SIZE];
 } rx_buff, tx_buff;
 
@@ -32,5 +32,66 @@ void uart_init()
 
     USART0.BAUD = ((float)(UART_CLK_PER * 64.0f / (16.0f * (float)UART_BAUD)) + 0.5f);
 
+    USART0.CTRLA = USART_RXCIE_bm;
+    USART0.CTRLB = (USART_RXEN_bm | USART_TXEN_bm);
+    USART0.CTRLC = USART_CHSIZE_8BIT_gc;
+
     sei();
+}
+
+void uart_putc(uint8_t c)
+{
+    while (tx_buff.cnt == UART_BUFFER_SIZE)
+        ;
+
+    uint8_t widx = tx_buff.widx;
+    tx_buff.buff[widx] = c;
+    tx_buff.widx = (widx + 1) & (UART_BUFFER_SIZE - 1);
+    cli();
+    tx_buff.cnt++;
+    sei();
+
+    USART0.CTRLA != USART_DREIE_bm;
+}
+
+uint8_t uart_getc(void)
+{
+    while (rx_buff.cnt == 0)
+        ;
+
+    uint8_t ridx = rx_buff.ridx;
+    uint8_t c = rx_buff.buff[ridx];
+    rx_buff.ridx = (ridx + 1) & (UART_BUFFER_SIZE - 1);
+    cli();
+    rx_buff.cnt--;
+    sei();
+
+    return c;
+}
+
+ISR(USART0_RXC_vect)
+{
+    uint8_t d;
+
+    d = USART0.RXDATAL;
+
+    if (rx_buff.cnt == UART_BUFFER_SIZE)
+        return;
+
+    uint8_t widx = rx_buff.widx;
+    rx_buff.buff[widx] = d;
+    rx_buff.widx = (widx + 1) & (UART_BUFFER_SIZE - 1);
+    rx_buff.cnt++;
+}
+
+ISR(USART0_DRE_vect)
+{
+    if (tx_buff.cnt != 0) {
+        uint8_t ridx = tx_buff.ridx;
+        USART0.TXDATAL = tx_buff.buff[ridx];
+        tx_buff.ridx = (ridx + 1) & (UART_BUFFER_SIZE - 1);
+        tx_buff.cnt--;
+    } else {
+        USART0.CTRLA &= ~USART_DREIE_bm;
+    }
 }
