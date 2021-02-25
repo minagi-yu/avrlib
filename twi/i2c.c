122a5774e904af90dd93169ba8f6b108ed527597
minagi-yu <39975793+minagi-yu@users.noreply.github.com>
2021-02-25 20:29:56 +0900

    アドレス送信処理を関数にまとめた

--- twi/i2c.c
+++ twi/i2c.c
@@ -3,11 +3,16 @@
 #define I2C_CLK_PER 3333333
 #define I2C_FSCL I2C_SM
 
-void i2c_init(int khz)
+#define I2C_DIR_WRITE 0
+#define I2C_DIR_READ 1
+
+void i2c_init(int hz)
 {
     TWI0.MBAUD = (I2C_CLK_PER / I2C_FSCL - 10) / 2;
 
-    TWI0.MCTRLA = (1 << TWI_ENABLE_bm);
+    TWI0.MCTRLA = TWI_ENABLE_bm;
+
+    TWI0.MSTATUS = TWI_BUSSTATE_IDLE_gc;
 }
 
 static void i2c_wait()
@@ -23,7 +28,23 @@ static void i2c_reset()
     TWI0.MSTATUS |= TWI_BUSSTATE_IDLE_gc;
 }
 
-uint_fast8_t i2c_send(uint8_t addr, void *data, size_t len)
+static uint_fast8_t i2c_transmit_addr(uint8_t addr, uint_fast8_t dir)
+{
+    do {
+        TWI0.MADDR = addr << 1 | dir;
+
+        i2c_wait();
+
+        if (TWI0.MSTATUS & (TWI_ARBLOST_bm | TWI_BUSERR_bm)) {
+            i2c_reset();
+            return 1;
+        }
+    } while (TWI0.MSTATUS & TWI_RXACK_bm); // NACK: rewrite address
+
+    return 0;
+}
+
+uint_fast8_t i2c_write(uint8_t addr, void *data, size_t len)
 {
     uint8_t *dp = data;
 
@@ -33,16 +54,8 @@ uint_fast8_t i2c_send(uint8_t addr, void *data, size_t len)
     i2c_reset();
     TWI0.MSTATUS |= (TWI_RIF_bm | TWI_WIF_bm);
 
-    do {
-        TWI0.MADDR = addr << 1;
-
-        i2c_wait();
-
-        if (TWI0.MSTATUS & (TWI_ARBLOST_bm | TWI_BUSERR_bm)) {
-            i2c_reset();
-            return 1;
-        }
-    } while (TWI0.MSTATUS & TWI_RXACK_bm); // NACK: rewrite address
+    if (i2c_transmit_addr(addr, I2C_DIR_WRITE))
+        return 1;
 
     // ACK: write data
     do {
@@ -74,16 +87,8 @@ uint_fast8_t i2c_read(uint8_t addr, void *data, size_t len)
     i2c_reset();
     TWI0.MSTATUS |= (TWI_RIF_bm | TWI_WIF_bm);
 
-    do {
-        TWI0.MADDR = addr << 1 | 1;
-
-        i2c_wait();
-
-        if (TWI0.MSTATUS & (TWI_ARBLOST_bm | TWI_BUSERR_bm)) {
-            i2c_reset();
-            return 1;
-        }
-    } while (TWI0.MSTATUS & TWI_RXACK_bm); // NACK: rewrite address
+    if (i2c_transmit_addr(addr, I2C_DIR_READ))
+        return 1;
 
     // ACK: read data
     do {
