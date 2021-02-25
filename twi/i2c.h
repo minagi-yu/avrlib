c6164361c93b28320f75a0af2ae8ce1457a37f7f
minagi-yu <39975793+minagi-yu@users.noreply.github.com>
2021-02-25 20:30:04 +0900

    プロトタイプ宣言追加

--- twi/i2c.h
+++ twi/i2c.h
@@ -4,8 +4,13 @@
 #include <avr/io.h>
 #include <stdint.h>
 
+/* I2C Speed Mode */
 #define I2C_SM 100000
 #define I2C_FM 400000
 #define I2C_FMPLUS 1000000
+
+void i2c_init(int khz);
+uint_fast8_t i2c_write(uint8_t addr, void *data, size_t len);
+uint_fast8_t i2c_read(uint8_t addr, void *data, size_t len);
 
 #endif
